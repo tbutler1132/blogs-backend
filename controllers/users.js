7e38b907315cbd4ab7eaede085b50e715c201fe9
Timothy Butler <tbutler1132@gmail.com>
2021-04-28 15:59:58 -0400

    add blog working

--- controllers/users.js
+++ controllers/users.js
@@ -43,3 +43,41 @@ export const createUser = async (req, res) => {
         res.status(400).json({message: error.message})
     }
 }
+
+// export const getUser = async (req, res) => {
+//     try {
+//        const user = User.findById(req.params.id)
+
+//         res.status(200).json(user)
+//     } catch (error) {
+//         res.status(404).json
+//     }
+
+// }
+
+// export const updateUser = async (req, res) => {
+//     try {
+//        const user = await User.findById(req.params.id)
+
+//         res.status(200).json(user)
+//     } catch (error) {
+//         res.status(404).json
+//     }
+// }
+
+export const updateUserPosts = async (req, res) => {
+    console.log(req.body)
+    const posts = req.body
+    const user = await User.findById(req.params.id)
+    user.posts.push(posts)
+    
+    
+    try {
+        await user.save()
+
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ import express from 'express'
 import {getUsers} from '../controllers/users.js'
 import {createUser} from '../controllers/users.js'
 import {getProfile} from '../controllers/users.js'
+import {updateUserPosts} from '../controllers/users.js'
 import {authenticateToken} from './auth.js'
 
 const router = express.Router()
@@ -11,5 +12,6 @@ const router = express.Router()
 router.get('/', getUsers)
 router.post('/', createUser)
 router.get('/profile', authenticateToken, getProfile )
+router.post('/update/posts/:id', updateUserPosts)
 
 export default router
